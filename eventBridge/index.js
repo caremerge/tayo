7df5c3956be7d5a3f71704483f1c6a65769c9942
mahnoorfatiima <mahnoor.fatima@careaxiom.com>
2022-11-21 02:24:39 +0500

    added scheduler for tayo

---
+++ eventBridge/index.js
@@ -0,0 +1,29 @@
+const aqi = require("../modules/aqi.js");
+
+async function postOnSlack(aqiResponse) {
+  let url =
+    "https://hooks.slack.com/services/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";
+  try {
+    response = await axios.post(
+      url,
+      {
+        text: `${aqiResponse}`,
+      },
+      {
+        headers: {
+          "Content-type": "application/json",
+        },
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+exports.aqiPoster = async function (cityList) {
+  let aqiList = "";
+  for (let city of cityList) {
+    aqiList += (await getAQI(city)) + "\n\n\n";
+  }
+  postOnSlack(aqiList);
+};
