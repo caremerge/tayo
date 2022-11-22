609edd99a456478834343a9d9ffa9ab728a24490
Muneeb <neebz@users.noreply.github.com>
2022-11-22 09:48:20 -0500

    some extra logs

--- eventBridge/index.js
+++ eventBridge/index.js
@@ -4,6 +4,9 @@ const axios = require("axios");
 async function postOnSlack(aqiResponse) {
   let url =
     process.env.AQI_WEBHOOK;
+  
+  console.log("posting to url:", url);
+  console.log("messge:", aqiResponse);
   try {
     response = await axios.post(
       url,
@@ -24,7 +27,9 @@ async function postOnSlack(aqiResponse) {
 exports.aqiPoster = async function (cityList) {
   let aqiList = "";
   for (let city of cityList) {
+    console.log("fetching for AQI for city:", city);
     aqiList += (await aqi.getAQI(city)) + "\n\n\n";
+    console.log("recieved AQI for city:", city);
   }
   postOnSlack(aqiList);
 };
